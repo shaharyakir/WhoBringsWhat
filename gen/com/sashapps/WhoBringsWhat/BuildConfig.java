b8b071a71a9befde207de4200dd77afafeb1e47e
shaharyakir <shahar.yakir@gmail.com>
2013-11-20 01:16:36 +0200

    v0.0.1

---
+++ gen/com/sashapps/WhoBringsWhat/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package com.sashapps.WhoBringsWhat;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
